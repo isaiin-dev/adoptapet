5f6983d47462f41a5ca42531c688b61ec88e99c2
ALEJANDRO <alejandro@albo.mx>
2021-03-06 11:14:30 -0600

    Remove unused files

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='production'
-export PORT=3000
-export SECRET='secret'
-export MONGO_URI='mongodb+srv://root:6tBJnNVCQLNKq1yR@cluster0.xhkcc.mongodb.net/users_database?retryWrites=true&w=majority'
